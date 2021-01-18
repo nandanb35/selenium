147415d69b4c956b3df491654d1af674e2c9e7e7
Nandan <nandanb35@gmail.com>
2021-01-18 10:12:22 +0530

    shell sort array changes

--- src/com/practice/sorting/ShellSort.java
+++ src/com/practice/sorting/ShellSort.java
@@ -1,16 +1,29 @@
 package com.practice.sorting;
 
+import java.util.Arrays;
+
 public class ShellSort {
 
 	public static void main(String[] args) {
-		int[] arr = { 4, 6, 45, 8, 1, -2, 199, -19 };
+		int[] arr = { 12, 6, 45, 8, 1, -2, 199, -19 };
 
 		for (int gap = arr.length / 2; gap > 0; gap /= 2) {
 
-			System.out.println(gap);
+			for (int i = gap; i < arr.length; i++) {
+
+				int newElement = arr[i];
+				int j = i;
+
+				while (j >= gap && arr[j - gap] > newElement) {
+					arr[j] = arr[j - gap];
+					j -= gap;
+				}
+				arr[j] = newElement;
+			}
 
 		}
 
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
