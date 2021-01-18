513827ff11814ffe5de2584ecca1f756fab45c95
Nandan <nandanb35@gmail.com>
2021-01-18 09:46:05 +0530

    first commit in sprint 1 branch

---
+++ src/com/practice/sorting/ShellSort.java
@@ -0,0 +1,16 @@
+package com.practice.sorting;
+
+public class ShellSort {
+
+	public static void main(String[] args) {
+		int[] arr = { 4, 6, 45, 8, 1, -2, 199, -19 };
+
+		for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+
+			System.out.println(gap);
+
+		}
+
+	}
+
+}
